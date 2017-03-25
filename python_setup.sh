f0b0685276756e264537b14f80f63fddc5112f8b
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-03-25 17:25:29 +0100

    Complete revamp of python_setup
    Additions:
    * Support for automatic pyenv install
    * Platform agnostic
    * pyenv-virtualenv automatic install
    * pyenv plugins auto install
    * Install latest versions of python2, python3 and anaconda3 (default)

--- python_setup.sh
+++ python_setup.sh
@@ -4,28 +4,69 @@ set -e 			# Exit on error
 set -o pipefail # Exit on pipe error
 set -x 			# Enable verbosity
 
-# Python from pyenv
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+# Check if pyenv is in path and run init, else install pyenv
+if command -v pyenv > /dev/null; then
+    eval "$(pyenv init -)";
+else
+    curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 
-# Install python2
-pyenv install 2.7.13
-# Install python3
-pyenv install 3.6.0
+    export PATH="$HOME/.pyenv/bin:$PATH"
+    echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+    echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshenv
 
-# Install packages 3
-pyenv global 3.6.0
-pip install -r pip.req -r conda.req
+    eval "$(pyenv init -)"
+    echo 'if command -v pyenv > /dev/null; then eval "$(pyenv init -); fi' >> ~/.bashrc
+    echo 'if command -v pyenv > /dev/null; then eval "$(pyenv init -); fi' >> ~/.zshenv
+fi
+
+# Check if pyenv-virtualenv is in path and run init, else install pyenv-virtualenv
+if command -v pyenv-virtualenv-init > /dev/null; then
+    eval "$(pyenv virtualenv-init -)";
+else
+    git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
+    eval "$(pyenv virtualenv-init -)";
+    echo 'if command -v pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -); fi' >> ~/.bashrc
+    echo 'if command -v pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -); fi' >> ~/.zshenv
+fi
+    
+
+# Install latest stable python2
+LATEST_PYTHON2="$(pyenv install -l | grep -oP '^\s+(2.*\d)' | sed 's/ //g' | tail -n 1)"
+pyenv install -s $LATEST_PYTHON2
+
+# Install latest stable python3
+LATEST_PYTHON3=$(pyenv install -l | grep -oP '^\s+(3.*\d)' | sed 's/ //g' | tail -n 1)
+pyenv install -s $LATEST_PYTHON3
 
 # Install full anaconda and packages
-pyenv install anaconda3-4.3.0
-pyenv global anaconda3-4.3.0
+LATEST_ANACONDA=$(pyenv install -l | grep -oP '^\s+(anaconda.*)' | sed 's/ //g' | tail -n 1)
+pyenv install -s $LATEST_ANACONDA
+pyenv global $LATEST_ANACONDA
+pip install -r pip.req
 conda install --yes --file conda.req
 
-# Set anaconda 3.6.0 as default
-pyenv global anaconda3-4.3.0
+# Set latest anaconda as default
+pyenv global $LATEST_ANACONDA
 
+# Create virtualenv folder
 mkdir ~/.virtualenvs
+
+# Check if pyenv doctor plugin is installed, install otherwise
+if [ ! -d "$(pyenv root)/plugins/pyenv-doctor" ]; then
+    git clone git://github.com/yyuu/pyenv-doctor.git ~/.pyenv/plugins/pyenv-doctor
+fi
+
+# Check if pyenv update plugin is installed, install otherwise
+if [ ! -d "$(pyenv root)/plugins/pyenv-update" ]; then
+    git clone git://github.com/yyuu/pyenv-update.git ~/.pyenv/plugins/pyenv-update
+fi
+
+# Check if pyenv which-ext plugin is installed, install otherwise
+if [ ! -d "$(pyenv root)/plugins/pyenv-which-ext" ]; then
+    git clone https://github.com/yyuu/pyenv-which-ext.git ~/.pyenv/plugins/pyenv-which-ext
+fi
+
 
 # Create Python Environment
 # conda-env create environment.yml
