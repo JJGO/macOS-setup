94bd0cd60587605bf424a04ff8dd89872ca8e39d
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-11-19 11:10:11 -0500

    Remove Dropbox from dotfile setup

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -7,11 +7,6 @@ set -x          # Enable verbosity
 # Clone dotfiles repo
 git clone "https://github.com/JJGO/dotfiles.git" ~/.dotfiles
 
-# Create symlink to Dropbox for backup
-# if [ ! -f ~/Dropbox/dotfiles ]; th
-#  ln -s ~/.dotfiles ~/Dropbox/dotfiles
-# fi
-
 # Use stow to symlink dotfiles
 cd ~/.dotfiles
 ./setup_all.sh
