d46b7236f4ea3713e912b40b87cc71ecab0d9b8f
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-11-19 15:25:06 -0500

    Add sudo reauth

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Look for developer tools (needed for Homebrew)
 xcode-select -p
 if [ $? -eq 0 ]; then
