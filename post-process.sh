590025407e75c53c61e2c6c0b96251415047c95e
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2017-12-21 17:09:16 +0100

    Re-enable Sublime Text settings repo

--- post-process.sh
+++ post-process.sh
@@ -24,9 +24,8 @@ SUBL_ROOT="$HOME/Library/Application Support/Sublime Text 3"
 curl 'https://packagecontrol.io/Package%20Control.sublime-package' -o "$SUBL_ROOT/Installed Packages/Package Control.sublime-package"
 
 # Clone my sublime settings into the User folder
-# TODO Fix sublime packages
-# rm -rf "$SUBL_ROOT/Packages/User"
-# git clone "https://github.com/JJGO/sublime-settings" "$SUBL_ROOT/Packages/User"
+rm -rf "$SUBL_ROOT/Packages/User"
+git clone "https://github.com/JJGO/sublime-settings" "$SUBL_ROOT/Packages/User"
 
 ### GIT CONFIG ###
 
