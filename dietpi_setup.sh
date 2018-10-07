75a7f417e39fb0ad84c517de6d7d3109ffcfe642
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2018-10-07 12:28:50 -0400

    Add cli utils for dietpi

--- dietpi_setup.sh
+++ dietpi_setup.sh
@@ -1,18 +1,42 @@
+# Build
+sudo apt-get install -y build-essential
+
+# Mount
+sudo apt-get install -y cifs-utils
+
+# Git
+sudo apt-get install -y git
+sudo apt-get install -y git-extras  # Extra git commands like git-ignore
+sudo apt-get install -y git-flow    # Git-Flow utils
+
+# Monitor
+sudo apt-get install -y htop        # Process monitor
+sudo apt-get install -y iotop       # I/O Monitor
+sudo apt-get install -y stress      # Test performance CPU, MEM, I/O...
+
+# CLI tools
+sudo apt-get install -y atool               # For zip, tar, 7z etc
+sudo apt-get install -y colordiff           # Diff with colors
+sudo apt-get install -y fdupes              # Find duplicates
+sudo apt-get install -y ffmpeg              # Video encoder
+sudo apt-get install -y highlight           # syntax color for ranger
+sudo apt-get install -y less
+sudo apt-get install -y lnav                # Log navigator
+sudo apt-get install -y mediainfo           # Media Analyzer
 # MOSH (mosh requires OpenSSH instead of Dropbear)
-sudo apt-get install mosh
+sudo apt-get install -y mosh
 sudo iptables -I INPUT 1 -p udp --dport 60000:61000 -j ACCEPT
+sudo apt-get install -y ncdu                # Free space navigator
+sudo apt-get install -y parallel            # Execute stdin pipes in parallel
+sudo apt-get install -y tmux
+sudo apt-get install -y tree                # Directory structure
+sudo apt-get install -y ranger              # CLI file explorer
+sudo apt-get install -y silversearcher-ag   # Better ack
+sudo apt-get install -y sshfs               # Mounting filesystems through ssh
+sudo apt-get install -y vim
+sudo apt-get install -y zsh
 
-# VIM
-sudo apt-get install vim
 
-# Build essential (gcc)
-sudo apt-get install build-essential
-sudo apt-get install cifs-utils
-
-# zsh & tmux
-sudo apt-get install zsh
-sudo apt-get install tmux
-sudo apt-get install less
 
 # Add shell to /etc/shells
 command -v zsh | sudo tee -a /etc/shells
@@ -50,9 +74,6 @@ source \$HOME/.cargo/env
 /DietPi/dietpi/login
 . /DietPi/dietpi/func/dietpi-globals
 EOT
-
-# Install ranger
-sudo apt-get install ranger
 
 
 # Install fzf
